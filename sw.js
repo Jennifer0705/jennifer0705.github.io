21c71b60e641358b8974a889f8aa55222897578f
Jennifer0705 <p2npng2tdb@privaterelay.appleid.com>
2026-09-09 09:42:57 +0800

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE='odie-manager-v1';
-const ASSETS=['./','./index.html','./icon-180.png','./icon-512.png'];
+const CACHE='odie-manager-v4';
+const ASSETS=['./','./index.html','./icon-180.png','./icon-512.png','./manifest.webmanifest'];
 self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)));self.skipWaiting()});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(keys=>Promise.all(keys.filter(k=>k!==CACHE).map(k=>caches.delete(k)))));self.clients.claim()});
 self.addEventListener('fetch',e=>{
